5a5b38a0f36b9e6987f7a47a4a12ed030792ee8d
SaulArt7 <saul.rivera@uptc.edu.co>
2022-08-12 10:33:52 -0500

    Modelo dparaColección de acces.model

--- frontend/src/app/services/products.service.ts
+++ frontend/src/app/services/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product } from '../models/product.model';
-
+ 
 @Injectable({
   providedIn: 'root',
 })
